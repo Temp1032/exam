7d27a81e20c23965a4f2abfaca1a1b22267e5529
Temp1032 <146944169+Temp1032@users.noreply.github.com>
2024-03-03 21:11:57 +0100

    java modif

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		// SpringApplication.run(DemoApplication.class, args);
+		int a;
+		int b;
+		int add;
+		int multi;
+		int div;
+
+		a = 5;
+		b = 10;
+		add = a+b;
+		multi = a*b;
+		div = a/b;
+
+		System.out.println(a + " plus " + b + " est egale a : " + add)
+		System.out.println(a + " multiplier par " + b + " est egale a : " + multi)
+		System.out.println(a + " diviser par " + b + " est egale a : " + div)
+
 	}
 
 }
